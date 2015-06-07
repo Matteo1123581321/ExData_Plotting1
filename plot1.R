017d8f6096863bcbaa38180a53cecd7ac32ba621
Matteo1123581321 <matthew.ladolce@gmail.com>
2015-06-07 15:39:58 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,13 @@
+# read the data form the .txt file
+# convert and assing the formating for data field
+# filter the date value based on the given dates
+
+dataFile <- "/home/matteo/Documents/R/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# Generate the graph
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
